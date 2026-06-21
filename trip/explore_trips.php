faa6b968127db00cdca6cfb03ccbc6a12b4e0f20
frhqistina06 <frhqistina06@gmail.com>
2026-06-21 22:31:55 +0800

    remove error HEAD

--- trip/explore_trips.php
+++ trip/explore_trips.php
@@ -70,7 +70,6 @@ $res = $conn->query("
                 <a href="join_trips.php?id=<?php echo $trip_id; ?>" class="btn btn-primary">Join Ride</a>
             </div>
         </div>
-<<<<<<< HEAD
 <?php endwhile; else: ?>
             <div class="empty-state">
                 <div class="empty-state-icon">🚗</div>
